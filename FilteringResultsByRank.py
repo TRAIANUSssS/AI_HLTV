6ddcf1018664bf62c7850dec61fdc608938e8fe6
TRAIANUSssS <52360284+TRAIANUSssS@users.noreply.github.com>
2024-06-28 10:47:01 +0300

    add rank stats to filtered matches

--- FilteringResultsByRank.py
+++ FilteringResultsByRank.py
@@ -27,10 +27,12 @@ class FilteringByRank:
             teams = self.rank_data[self.current_rank_date]["teams"]
             for match_info in self.results_data[result_date]:
                 if match_info["team1"] in teams or match_info["team2"] in teams:
-                    print(match_info)
-
+                    rank1, points1, rank2, points2 = self.get_rank(match_info)
                     match_info["score1"], match_info["score2"] = int(match_info["score1"]), int(match_info["score2"])
+                    match_info["rank1"], match_info["rank2"] = rank1, rank2
+                    match_info["points1"], match_info["points2"] = points1, points2
                     self.filtered_results_data[result_date].append(match_info)
+                    print(match_info)
 
                     matches_counter += 1
                     total_score = int(match_info["score1"]) + int(match_info["score2"])
@@ -44,6 +46,20 @@ class FilteringByRank:
         print("matches count:", matches_counter, "maps count:", maps_counter)
         print("total matches count:", total_matches_counter, "total maps count:", total_maps_counter)
         self.save_filtered_results_stats()
+
+    def get_rank(self, match_info):
+        ranks_data = []
+        teams = [match_info["team1"], match_info["team2"]]
+        for team in teams:
+            if team in self.rank_data[self.current_rank_date]["teams"]:
+                rank = self.rank_data[self.current_rank_date]["placements"][team]
+                points = self.rank_data[self.current_rank_date]["points"][team]
+            else:
+                rank = 40
+                points = 10
+
+            ranks_data += [rank, points]
+        return ranks_data
 
     def preparing_data(self):
         self.open_files()
