c207cf25878a607c5c0f69083877ba96624b116c
TRAIANUSssS <52360284+TRAIANUSssS@users.noreply.github.com>
2024-07-22 17:27:51 +0300

    edit bad files names

--- AddDaysInTeamToMatchData.py
+++ AddDaysInTeamToMatchData.py
@@ -3,20 +3,25 @@ import pickle
 
 import Constants as con
 
+
 def get_matches_data():
     return pickle.load(open(f'{con.MAIN_PATH}Data/Matches/MatchesData/match_complete.pkl', "rb"))
+
 
 class AddDaysInTeam:
     def __init__(self):
         self.all_matches_data = get_matches_data()
 
     def go_every_match(self):
-        for key in list(self.all_matches_data.keys())[:5]:
+        self.edit_bad_files_names()
+        for key in list(self.all_matches_data.keys())[:]:
             players_list = self.all_matches_data[key]["players"]
             players_days_in_team = []
             date = self.all_matches_data[key]["date"]
             for player_index, player_name in enumerate(players_list):
-                current_team = self.all_matches_data[key]["team1"] if player_index < 5 else self.all_matches_data[key]["team2"]
+                player_name = self.edit_player_name(player_name)
+                current_team = self.all_matches_data[key]["team1"] if player_index < 5 else self.all_matches_data[key][
+                    "team2"]
                 file_name = f'{con.MAIN_PATH}Data/PlayersStats/AllMatches/all_maps_{player_name}.pkl'
                 if os.path.exists(file_name):
                     players_date_info = pickle.load(open(file_name, "rb"))
@@ -28,11 +33,27 @@ class AddDaysInTeam:
                         date_index += 1
                     # print(current_team, players_date_info[players_dates[date_index]])
                     if current_team.replace(" ", "") == players_date_info[players_dates[date_index]].replace(" ", ""):
-                        players_days_in_team.append((date-players_dates[date_index]) // 86400)
+                        players_days_in_team.append((date - players_dates[date_index]) // 86400)
                     else:
                         players_days_in_team.append(0)
                     # print(player_name, players_days_in_team[-1], current_team, players_date_info[players_dates[date_index]], date, players_dates[date_index])
                 else:
                     players_days_in_team.append(-1)
+                    print(player_name, "https://www.hltv.org/matches/" + key.replace("_", "/", 1))
 
             print(players_days_in_team, key)
+
+    def edit_player_name(self, player_name):
+        player_name = player_name.replace("-", "")
+        player_name = player_name.replace(" ", "").replace("Š", "").replace("ś", "").replace("ó", "")
+        player_name = player_name.replace("_", "").replace("ı", "")
+        return player_name
+
+    def edit_bad_files_names(self):
+        files = os.listdir(f'{con.MAIN_PATH}Data/PlayersStats/AllMatches')
+        for file in files:
+            if file.count(" ") or file.count("-"):
+                data = pickle.load(open(f'{con.MAIN_PATH}Data/PlayersStats/AllMatches/{file}', "rb"))
+                pickle.dump(data, open(f'{con.MAIN_PATH}Data/PlayersStats/AllMatches/{file.replace("-", "")}', "wb"))
+                os.remove(f'{con.MAIN_PATH}Data/PlayersStats/AllMatches/{file}')
+                print(file)
